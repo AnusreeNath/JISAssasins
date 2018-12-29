0c4cf847fa38f599afbbf2d2db5c3f104c3c90bb
AnusreeNath <46235873+AnusreeNath@users.noreply.github.com>
2018-12-29 14:40:23 +0530

    Add files via upload

---
+++ anu8.py
@@ -0,0 +1,6 @@
+#question no 25 by anusree
+ch=input("enter a character ")
+if(ch>='a' and ch<='z') or (ch>='A' and ch<='Z'):
+   print("The given character ",ch,"is an alphabet")
+else:
+    print("The given character ",ch,"is a digit")
