98d48a5344f6140da7d66c2b9e7d640f49d3fb83
AnusreeNath <46235873+AnusreeNath@users.noreply.github.com>
2018-12-29 16:04:35 +0530

    Add files via upload

---
+++ anu13.py
@@ -0,0 +1,20 @@
+#question no 23 by anusree
+string=input("enter the string")
+count=0
+for i in string:
+    count=count+1
+print("length of stringis ",count)
+list=string.split(" ")
+list.reverse()
+print(string[::-1])
+string1=input("enter thestring")
+s3=string+string1
+print(s3)
+if(string==string1):
+    print("they are equal are equal ")
+else:
+    print("they are not equal")
+if (string1==string) :
+    exit()
+else:
+    print (string1)
