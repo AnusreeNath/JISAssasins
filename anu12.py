61954499ce9d2d117ae3f346616e80ed222c139d
AnusreeNath <46235873+AnusreeNath@users.noreply.github.com>
2018-12-29 15:51:57 +0530

    Add files via upload

---
+++ anu12.py
@@ -0,0 +1,8 @@
+#question no 19 by anusree
+a=int(input('enter the 1st sides value of triangle'))
+b=int(input('enter the 2nd sides value of triangle'))
+c=int(input('enter the 3rd sides value of triangle'))
+if (c>=a+b):
+    print("this is a valid triangle\n")
+else:
+    print("this is an invalid triangle\n")
